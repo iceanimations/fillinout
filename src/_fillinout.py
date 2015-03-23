de7f92548831a6b67df38b2bc3a669d68890b2e1
Qurban <qurban_ali36@yahoo.com>
2015-03-23 13:46:57 +0500

    cache support added

--- src/_fillinout.py
+++ src/_fillinout.py
@@ -5,36 +5,49 @@ Created on Aug 28, 2014
 '''
 
 import pymel.core as pc
-import site
-site.addsitedir(r'r:/pipe_repo/users/qurban/utilities')
 import appUsageApp
 
 def fill():
     
-    cam = pc.ls(sl=True, dag=True, type='camera')
+    sel = pc.ls(sl=True)
     
-    if not cam:
-        pc.warning('No camera selected...')
+    if not sel:
+        pc.warning('Select a mesh or camera')
         return
     
-    if len(cam) > 1:
-        pc.warning('More than one cameras selected...')
+    if len(sel) > 1:
+        pc.warning('Select only one camera or mesh')
         return
-    
-    cam = cam[0]
-    animCurves = pc.listConnections(cam.firstParent(), scn=True, d=False, s=True)
-    if not animCurves:
-        pc.warning('No animation found on the selected camera...')
+    try:
+        obj = sel[0].getShape(ni=True)
+    except:
+        pc.warning('Selection should be camera or mesh')
         return
-    
-    frames = pc.keyframe(animCurves[0], q=True)
-    if not frames:
-        pc.warning('No keys found on the selected camera...')
+    if type(obj) == pc.nt.Mesh:
+        try:
+            cache = obj.history(type='cacheFile')[0]
+        except IndexError:
+            pc.warning('No cache found on the selected object')
+            return
+        start = cache.sourceStart.get()
+        end = cache.sourceEnd.get()
+    elif type(obj) == pc.nt.Camera:
+        animCurves = pc.listConnections(obj.firstParent(), scn=True, d=False, s=True)
+        if not animCurves:
+            pc.warning('No animation found on the selected camera...')
+            return
+        
+        frames = pc.keyframe(animCurves[0], q=True)
+        if not frames:
+            pc.warning('No keys found on the selected camera...')
+            return
+        
+        start = frames[0]
+        end = frames[-1]
+    else:
+        pc.warning('Selection should be camera or mesh')
         return
-    
-    start = frames[0]
-    end = frames[-1]
-    
+
     pc.playbackOptions(minTime=start)
     pc.setAttr("defaultRenderGlobals.startFrame", start)
     pc.playbackOptions(maxTime=end)
